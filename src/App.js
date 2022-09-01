05771d01d50f0c8d027b73e8c7cea959c377ad9c
Heitor Pastorelli <heitorpessoal7@gmail.com>
2022-09-01 18:15:48 -0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Content">
+      <nav>
+        <div>
+          <img className="react-logo" src={logo} />
+          <h1 className="react-logo-text">ReactFacts</h1>
+        </div>
+        <h3 className="project-name">React Course - Project 1</h3>
+      </nav>
+      <main>
+        <h1 className="title">Fun Facts About React</h1>
+        <ul className="facts-list">
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
     </div>
   );
 }
